f1c98e7b8e0869e2d77c6f6857dba50c9320de6a
gwangwoo <rhkddn5161@naver.com>
2022-06-26 18:57:07 +0900

    boj_25288

---
+++ src/_boj_25288.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _boj_25288 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    String str = br.readLine();
+    StringBuilder sb = new StringBuilder();
+    while(N -- > 0) sb.append(str);
+    System.out.println(sb);
+  }
+}
