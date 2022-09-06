f4a2202275261d37780020e9fb25f431b3dd677f
gwangwoo <rhkddn5161@naver.com>
2022-09-07 00:18:49 +0900

    boj_24479 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ src/_boj_24479.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class _boj_24479 {
+  static int V,E,S;
+  static List<Integer>[] arl;
+  static int visited[];
+  static int N = 1;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
+    S = Integer.parseInt(st.nextToken());
+    arl = new ArrayList[V+1];
+    visited = new int[V+1];
+    for(int i = 1 ; i <= V ; i++) arl[i] = new ArrayList<>();
+    for(int i = 0 ; i < E ; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      arl[u].add(v);
+      arl[v].add(u);
+    }
+    for(int i = 1 ; i <= V ; i++) Collections.sort(arl[i]);
+
+    dfs(S);
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    for(int i = 1 ; i <= V ; i++) {
+      bw.write(visited[i]+"\n");
+    }
+    bw.flush();
+    bw.close();
+  }
+
+  static void dfs(int node) {
+    if(visited[node] != 0) return;
+    visited[node] = N++;
+
+    for(int y : arl[node]) {
+      dfs(y);
+    }
+  }
+}
