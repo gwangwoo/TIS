dc77112d7bd834e529e8004112339e28b8bd8e18
gguu <rhkddn5161@naver.com>
2023-02-12 16:02:17 +0900

    _boj_12865 평범한 배낭

---
+++ src/_boj_12865.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _boj_12865 {
+    static int N, K;
+    static int arr[][];
+    static int dp[][];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1][2];
+        dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (arr[i][0] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i-1][j - arr[i][0]] + arr[i][1]);
+            }
+        }
+        System.out.println(dp[N][K]);
+    }
+}
